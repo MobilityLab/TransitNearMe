20fc71f726bbea35ba8a1fc5d12d6d75feb6b90b
Andy Chosak <andy@localstandout.com>
2011-10-27 11:56:28 -0400

    Deployment changes.

--- localtransit/wsgi_app.py
+++ localtransit/wsgi_app.py
@@ -1,4 +1,5 @@
-import os, django.core.handlers.wsgi
+import os, sys, django.core.handlers.wsgi
 
+sys.path.append(os.path.dirname(__file__))
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 application = django.core.handlers.wsgi.WSGIHandler()
