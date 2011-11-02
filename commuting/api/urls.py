df5bdfa8b2d856574d6f0f34cfdcf12bb368805e
Andy Chosak <andy@localstandout.com>
2011-11-02 15:19:46 -0400

    Removed old "nearby" view.

--- commuting/api/urls.py
+++ commuting/api/urls.py
@@ -2,6 +2,5 @@ from django.conf.urls.defaults import patterns, include, url
 from api.views import *
 
 urlpatterns = patterns('api.views',
-	url(r'^nearby$', nearby, name='nearby'),
 	(r'^stops$', NearbyStopsView.as_view()),	
 )
