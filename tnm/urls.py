4e08a3c92e462f70f2d75878eee46c3422f5cac6
Andy Chosak <andy@localstandout.com>
2011-12-02 14:27:19 -0500

    Adding favicon.ico.

--- tnm/urls.py
+++ tnm/urls.py
@@ -4,7 +4,9 @@ from django.views.generic.simple import direct_to_template
 
 admin.autodiscover()
 
-urlpatterns = patterns('')
+urlpatterns = patterns('',
+    (r'^favicon\.ico$', 'django.views.generic.simple.redirect_to', {'url': '/static/images/favicon.ico'}),
+)
 
 urlpatterns += patterns('',
     url(r'^admin/', include(admin.site.urls)),
