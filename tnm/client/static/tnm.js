63891e29155f7788a7a99c75edc4505b56f8036b
Andy Chosak <andy@localstandout.com>
2011-12-08 15:27:08 -0500

    Fixing tabs in tnm.js.

--- tnm/client/static/tnm.js
+++ tnm/client/static/tnm.js
@@ -1,15 +1,15 @@
 $(function() {
-	var mapElementName = 'map',
-		getRadius = function()  { 
-			return $('input:radio[name=radius-button]:checked').val(); 
-		},
-		map = Transit.addMap(
-			'Leaflet', 
-			mapElementName,
-			{ 
-				'lat': 38.89706, 
-				'lng': -77.07092, 
-				'radius': getRadius() });
+    var mapElementName = 'map',
+        getRadius = function()  { 
+            return $('input:radio[name=radius-button]:checked').val(); 
+        },
+        map = Transit.addMap(
+            'Leaflet', 
+            mapElementName,
+            { 
+                'lat': 38.89706, 
+                'lng': -77.07092, 
+                'radius': getRadius() });
 
     map.addCallback({
         types: ['dblclick'],
@@ -18,41 +18,41 @@ $(function() {
         }
     });
 
-	map.addCallback({
-		types: ['center'],
-		apiCall: Transit.API.getNearby,
-		extraParams: { 'radius_m': getRadius },
-		before: function(e) {
-			map.radius(e.latlng, getRadius());
+    map.addCallback({
+        types: ['center'],
+        apiCall: Transit.API.getNearby,
+        extraParams: { 'radius_m': getRadius },
+        before: function(e) {
+            map.radius(e.latlng, getRadius());
             map.overlay('nearby');
-			$.mobile.showPageLoadingMsg();
-		},
-		after: function(e, data) {
-			map.radius(e.latlng, getRadius());
-			map.overlay('nearby', data);
-			$(window).resize();
-			$.mobile.hidePageLoadingMsg();
-		}
-	});	
-	
-	$('#options').live('pagehide', function(e) {
-		map.center();
-	});
+            $.mobile.showPageLoadingMsg();
+        },
+        after: function(e, data) {
+            map.radius(e.latlng, getRadius());
+            map.overlay('nearby', data);
+            $(window).resize();
+            $.mobile.hidePageLoadingMsg();
+        }
+    }); 
+    
+    $('#options').live('pagehide', function(e) {
+        map.center();
+    });
 
     var do_resize = function() {
-		var viewport_height = window.innerHeight,
-			header = $(".header:visible"),
-			content = $(".content:visible");
-		content.height(viewport_height - header.outerHeight());
+        var viewport_height = window.innerHeight,
+            header = $(".header:visible"),
+            content = $(".content:visible");
+        content.height(viewport_height - header.outerHeight());
     };
 
-	$(window).bind("orientationchange", function() {
+    $(window).bind("orientationchange", function() {
         scroll(0, 0);
         do_resize();
     });
     $(window).bind("resize pageshow", do_resize);
-	
-	Transit.tryGeolocating(
-		function(latlng) { map.center(latlng); },
-		function(error) { map.center(); });
+    
+    Transit.tryGeolocating(
+        function(latlng) { map.center(latlng); },
+        function(error) { map.center(); });
 });
