7cffa3fd8b113c949179fac51e5cc8dfd976646e
Andy Chosak <andy@localstandout.com>
2012-01-09 15:27:45 -0500

    Client properly handling new backend format.

--- tnm/client/static/transit.js
+++ tnm/client/static/transit.js
@@ -1,54 +1,53 @@
+// Define the Transit namespace.
 (function(x) {
-	x.Transit = {};
+    x.Transit = {};
 })(this);
 
 // Common library functions.
 Transit.addMap = function(type, element, options) {
-	switch(type)
-	{
-		case 'Leaflet': return new Transit._leafletMap(element, options);
-		default: throw 'Unknown map type.';
-	};
+    switch(type)
+    {
+        case 'Leaflet': return new Transit._leafletMap(element, options);
+        default: throw 'Unknown map type.';
+    };
 };
 
 Transit.tryGeolocating = function(success, failure) {
-	if (navigator && navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(
-			function(pos) {
-				success && success({
-					'lat': pos.coords.latitude,
-					'lng': pos.coords.longitude
-				});
-			},
-			function(error) {
-				failure && failure(error);
-			}
-		);
-	} else {
+    if (navigator && navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+            function(pos) {
+                success && success({
+                    'lat': pos.coords.latitude,
+                    'lng': pos.coords.longitude
+                });
+            },
+            function(error) {
+                failure && failure(error);
+            });
+    } else {
         failure && failure();
     }
 };
 
 Transit.getIcon = function() {
     var icons = [];
-    return function(routeTypes) {
-        if (1 != routeTypes.length) {
-            var routeType = -1; 
-        } else {
-            var routeType = routeTypes[0];
+    return function(services) {
+        var route_type = -1;
+        for (var i in services) {
+            route_type = services[i].route.route_type;
         }
 
-        if (!icons[routeType]) {
-            switch (routeType.toString()) {
+        if (!icons[route_type]) {
+            switch (route_type.toString()) {
                 case '0': filename = '23_bus_inv_thumb.gif'; break;
                 case '1': filename = '25_railtransportation_inv_thumb.gif'; break;
                 case '2': filename = '25_railtransportation_inv_thumb.gif'; break;
                 case '3': filename = '23_bus_inv_thumb.gif'; break;
                 default: filename = 'marker.png'; break;
             }
-            icons[routeType] = new Transit._leafletMap.TransitIcon('/static/images/' + filename);
+            icons[route_type] = new Transit._leafletMap.TransitIcon('/static/images/' + filename);
         }
-        return icons[routeType];
+        return icons[route_type];
     };
 }();
 
@@ -96,216 +95,307 @@ Transit.decodePolyline = function(polyline) {
 
 // API calls.
 Transit.API = {
-	_call: function(urlTmpl) {
-		return function(params, callback) {
-			var url = urlTmpl, d;
-			for (i in params) {
-				if ($.isFunction(params[i])) {
-					params[i] = params[i]();
-				}
-				url = url.replace(RegExp('\\{'+i+'\\}', 'gi'), params[i]);
-			}
-			d = $.getJSON(url);
-			callback && d.success(callback);
-		};
-	}
+    _call: function(urlTmpl) {
+        return function(params, callback) {
+            var url = urlTmpl, d;
+            for (i in params) {
+                if ($.isFunction(params[i])) {
+                    params[i] = params[i]();
+                }
+                url = url.replace(RegExp('\\{'+i+'\\}', 'gi'), params[i]);
+            }
+            d = $.getJSON(url);
+            callback && d.success(callback);
+        };
+    }
 };
 
 Transit.API.getNearby = Transit.API._call(
-	'/api/nearby?lng={lng}&lat={lat}&radius_m={radius_m}');
+    '/api/nearby?lng={lng}&lat={lat}&radius_m={radius_m}');
 
 // Leaflet Map wrapper.
 Transit._leafletMap = function(element, options) {
-	var map = new L.Map(element, { attributionControl: false }),
-		latlng = new L.LatLng(options.lat, options.lng),
-		url = 'http://otile{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png',
-		mqosm = new L.TileLayer(
-			url,
-			{
-				subdomains: '1234',
-				maxZoom: 18
-			}
-		);
-	map.doubleClickZoom.disable();
-	map.setView(latlng, 15);
-	map.addLayer(mqosm);
-	
-	this._map = map;
-	this._layers = { 'OSM': mqosm };
-	this._geolayers = {};
+    var map = new L.Map(element, { attributionControl: false }),
+        latlng = new L.LatLng(options.lat, options.lng),
+        url = 'http://otile{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png',
+        mqosm = new L.TileLayer(
+            url,
+            {
+                subdomains: '1234',
+                maxZoom: 18
+            });
+    map.doubleClickZoom.disable();
+    map.setView(latlng, 15);
+    map.addLayer(mqosm);
+        
+    this.layers = { 'OSM': mqosm };
+    this.hiddenLayers = {};
+       
+    this.segments = {};
+    this.stops = {};
+    this.routes = {};
+    this.services = {};
 
-	if (options.radius) {
-		this._layers['radius'] = new L.Circle(latlng, options.radius, { 
-			clickable: false,
-			weight: 1,
-			fillOpacity: 0.1
-		});
-	}
+    this.map = map;
+    this.map._wrapper = this; 
 
-	map.on('popupopen', function(e) {
-		var src = e.popup._source;
-        this._hiddenLayers = [];
-		for (lid in this._layers)
-		{
-			var layer = this._layers[lid];
+    if (options.radius) {
+        this.layers['radius'] = new L.Circle(latlng, options.radius, { 
+            clickable: false,
+            weight: 1,
+            fillOpacity: 0.1
+        });
+    }
 
-            if (src.stop) {
-                if ((layer.stop && src.stop != layer.stop) ||
-                    (layer.service && -1 == $.inArray(layer.service, src.stop.services))) {
-                    this._hiddenLayers.push(layer);
-                    this.removeLayer(layer);
+    map.on('popupopen', function(e) {
+        var src = e.popup._source,
+            visibleLayers = {},
+            hiddenLayers = [],
+            i, j, layer, stop, service, segment;
+   
+        if (!src._overlayID) {
+            return;
+        }
+ 
+        if (src.stop) {
+            visibleLayers[src.stop.layer._leaflet_id] = src.stop.layer;
+            for (i in src.stop.services) {
+                service = src.stop.services[i];
+                for (j in service.segments) {
+                    segment = service.segments[j];
+                    visibleLayers[segment.layer._leaflet_id] = segment.layer;
+                }
+            } 
+        }
+        if (src.segment) {
+            visibleLayers[src.segment.layer._leaflet_id] = src.segment.layer;
+            for (i in src.segment.services) {
+                service = src.segment.services[i];
+                visibleLayers[service.stop.layer._leaflet_id] = service.stop.layer;
+                for (j in service.segments) {
+                    segment = service.segments[j];
+                    visibleLayers[segment.layer._leaflet_id] = segment.layer;
                 }
             }
-            if (src.service) {
-                if ((layer.service && src.service != layer.service) ||
-                    (layer.stop && -1 == $.inArray(src.service, layer.stop.services))) {
-                    this._hiddenLayers.push(layer);
-                    this.removeLayer(layer);
+        }
+
+        for (i in this._layers)
+        {
+            layer = this._layers[i];
+            if (layer._overlayID && layer._overlayID == src._overlayID) {
+                if (!(layer._leaflet_id in visibleLayers)) {
+                    hiddenLayers.push(layer);
                 }
             }
-		}
-	});
-	map.on('popupclose', function(e) {
-		if (this._hiddenLayers) {
-			for (rl in this._hiddenLayers) {
-				this.addLayer(this._hiddenLayers[rl]);
-			}
-		}
-	});
+        }
+
+        for (i in hiddenLayers) {
+            this.removeLayer(hiddenLayers[i]);
+        }
+        this._wrapper.hiddenLayers[src._overlayID] = hiddenLayers;
+    });
+    map.on('popupclose', function(e) {
+        var src = e.popup._source, 
+            hiddenLayers, i;
+
+        if (!src._overlayID) {
+            return;
+        }
+
+        hiddenLayers = this._wrapper.hiddenLayers[src._overlayID];
+        for (i in hiddenLayers) {
+            this.addLayer(hiddenLayers[i]);
+        }
+    });
 };
 
 Transit._leafletMap.prototype.center = function(latlng) {
-	latlng && this._map.setView(latlng, this._map.getZoom());
-	this._map.fire('center', { latlng: latlng });
+    latlng && this.map.setView(latlng, this.map.getZoom());
+        this.map.fire('center', { latlng: latlng });
 };
 
 Transit._leafletMap.prototype.addCallback = function(options) {
-	for (i in options.types) {
-		this._map.on(options.types[i], function(e) {
-			if (!e.latlng) {
-				e.latlng = e.target.getCenter();
-			}
-			options.before && options.before(e);
-			if (options.apiCall) {
-				options.apiCall($.extend(
-					{}, 
-					options.extraParams, 
-					{
-						'lng': e.latlng.lng,
-						'lat': e.latlng.lat
-					}),
-					function(data) {
-						options.after && options.after(e, data);
-					}
-				);
-			} else {
-				options.after && options.after(e);
-			}
-		});
-	}
+    for (i in options.types) {
+        this.map.on(options.types[i], function(e) {
+            if (!e.latlng) {
+                e.latlng = e.target.getCenter();
+            }
+            options.before && options.before(e);
+            if (options.apiCall) {
+                options.apiCall($.extend(
+                    {}, 
+                    options.extraParams, 
+                    {
+                        'lng': e.latlng.lng,
+                        'lat': e.latlng.lat
+                    }),
+                    function(data) {
+                        options.after && options.after(e, data);
+                    });
+            } else {
+                options.after && options.after(e);
+            }
+        });
+    }
 };
 
 Transit._leafletMap.prototype.overlay = function(overlayID, overlay) {
-	var oldLayer = this._layers[overlayID];
-    oldLayer && this._map.removeLayer(oldLayer);
-    delete this._layers[overlayID];
+    var oldLayer = this.layers[overlayID],
+        layers = new L.LayerGroup(),
+        i, j, num;
+
+    oldLayer && this.map.removeLayer(oldLayer);
+    delete this.layers[overlayID];
    
     if (!overlay) {
         return;
     }
 
-    var layerGroup = new L.LayerGroup(),
-        services = overlay.services,
-        stops = overlay.stops,
-        routes = overlay.routes;
- 
-    for (var i in services) {
-        var service = services[i],
-            stop = stops[service.stop],
-            route = routes[service.route];
+    this.stops = {};
+    this.routes = {};
+    this.segments = {};
+    this.services = {};
+
+    // Update map containers with new data.
+    for (i in overlay.stops) {
+        stop = overlay.stops[i];
+        stop.services = {};
+        this.stops[stop.id] = stop;
+    }
+
+    for (i in overlay.routes) {
+        route = overlay.routes[i];
+        this.routes[route.id] = route;
+    }
+
+    for (i in overlay.segments) {
+        segment = overlay.segments[i];
+        segment.line = Transit.decodePolyline(segment.line_encoded);
+        segment.services = {};
+        segment.destinations = {};
+        this.segments[segment.id] = segment;
+    }
+
+    for (i in overlay.services) {
+        service = overlay.services[i];
+        service.route = this.routes[service.route];
+        service.description = service.route.agency + ' ' + service.route.short_name + ' to ' + service.destination;
         
-        if (!stop.services) {
-            stop.services = [];
-        }
-        stop.services.push(service);
+        service.stop = this.stops[service.stop];
+        service.stop.services[service.id] = service;
 
-        if (!stop.service_types) {
-            stop.service_types = [];
-        }
-        if (-1 == $.inArray(route.route_type, stop.service_types)) {
-            stop.service_types.push(route.route_type);
+        for (j in service.segments) {
+            segment = this.segments[service.segments[j]];
+            segment.services[service.id] = service;
+            
+            if (!(service.route.id in segment.destinations)) {
+                segment.destinations[service.route.id] = [];
+            }
+            segment.destinations[service.route.id].push(service.destination);
+
+            service.segments[j] = segment;
         }
 
-        if (!stop.routes) {
-            stop.routes = [];
-        }
-        if (-1 == $.inArray(route, stop.routes)) {
-            stop.routes.push(route);
-        }
-    }
-    
-    for (var stop_id in stops) {
-        stop = stops[stop_id];
-        var icon = Transit.getIcon(stop.service_types),
-            stop_marker = new L.Marker(stop.location, { icon: icon });
-        stop_marker.stop = stop;
-
-        var popup_content = '<p>' + stop.name + '</p><ul>';
-        for (var i in stop.services) {
-            var service = stop.services[i]; 
-            popup_content += '<li>' + routes[service.route].agency + ' ' + routes[service.route].short_name + ' to ' + service.destination + '</li>';
-        }
-        popup_content += '</ul>';
-        stop_marker.bindPopup(popup_content);
-        layerGroup.addLayer(stop_marker); 
+        this.services[service.id] = service;
     }
 
-    for (var stop_id in stops) {
-        stop = stops[stop_id];
-        for (var i in stop.services) {
-            var service = stop.services[i],
-                color = routes[service.route].color;
+    // Build segment lines and popups.
+    for (i in this.segments) {
+        var num_routes = 0, popup_content = '', color;
+
+        segment = this.segments[i];
+
+        for (j in segment.destinations) {
+            num_routes++;
+        }
+
+        if (num_routes > 1) {
+            popup_content += '<ul>';
+        }
+        for (j in segment.destinations) {
+            route = this.routes[j];
+            
+            if (num_routes > 1) {
+                popup_content += '<li>';
+            }
+            popup_content += route.agency + ' ' + route.short_name + ' to ' + segment.destinations[route.id].join(', ');
+            if (num_routes > 1) {
+                popup_content += '</li>';
+            }
+            
+            // A single line can have multiple colors.
+            // Pick one arbitrarily. Ideally the line should be dashed,
+            // or multiple parallel lines should be shown.
+            color = route.color;
+        
+            // Correct color.
             if (!color) {
                 color = '#333333';
             } else {
                 color = '#' + color;
             }
-            for (var j in service.segments) {
-                var segment = service.segments[j],
-                    latlngs = Transit.decodePolyline(segment.points);
-                    service_line = new L.Polyline(latlngs, { color: color, opacity: 0.8 });
-                service_line.service = service;
-
-                var popup_content = routes[service.route].agency + ' ' + routes[service.route].short_name + ' to ' + service.destination;
-                service_line.bindPopup(popup_content);
-                layerGroup.addLayer(service_line);
-            }
         }
+        if (num_routes > 1) {
+            popup_content += '</ul>';
+        }
+    
+        line = new L.Polyline(segment.line, { color: color, opacity: 0.8 });
+        line.segment = segment;
+        line.bindPopup(popup_content);
+        line._overlayID = overlayID;
+        segment.layer = line;
+        layers.addLayer(line);
+    }
+   
+    // Build stop icons and popups.
+    for (i in this.stops) {
+        stop = this.stops[i];
+
+        var icon = Transit.getIcon(stop.services),
+            stop_marker = new L.Marker(stop.location, { icon: icon }),
+            popup_content = '<p>' + stop.name + '</p>',
+            num_services = 0;
+
+        for (j in stop.services) {
+            num_services++;
+        }
+
+        popup_content += '<ul>';
+        for (j in stop.services) {
+            popup_content += '<li>';
+            popup_content += stop.services[j].description;
+            popup_content += '</li>';
+        }
+        popup_content += '</ul>';
+        
+        stop_marker.stop = stop;
+        stop_marker.bindPopup(popup_content);
+        stop_marker._overlayID = overlayID;
+        stop.layer = stop_marker;
+        layers.addLayer(stop_marker);
     }
 
-    oldLayer && this._map.removeLayer(oldLayer);
-    this._layers[overlayID] = layerGroup;
-    this._map.addLayer(layerGroup);
-    this._map._services = services;
-    this._map._stops = stops;
-    this._map._routes = routes;
+    oldLayer && this.map.removeLayer(oldLayer);
+    this.layers[overlayID] = layers;
+    this.hiddenLayers[overlayID] = [];
+    this.map.addLayer(layers);
 }
 
 Transit._leafletMap.prototype.radius = function(latlng, radius_m) {
-	var radius = this._layers['radius'];	
-	if (radius) {
-		radius.setLatLng(latlng);
-		radius_m && radius.setRadius(radius_m);
-		if (!this._map.hasLayer(radius)) {
-			this._map.addLayer(radius);
-		}
-	}
+    var radius = this.layers['radius'];    
+    if (radius) {
+        radius.setLatLng(latlng);
+        radius_m && radius.setRadius(radius_m);
+        if (!this.map.hasLayer(radius)) {
+            this.map.addLayer(radius);
+        }
+    }
 }
 
 Transit._leafletMap.TransitIcon = L.Icon.extend({
-	iconUrl: '',
-	shadowUrl: '',
-	iconSize: new L.Point(25, 25),
-	iconAnchor: new L.Point(13, 13),
-	popupAnchor: new L.Point(0, -7)
+    iconUrl: '',
+    shadowUrl: '',
+    iconSize: new L.Point(25, 25),
+    iconAnchor: new L.Point(13, 13),
+    popupAnchor: new L.Point(0, -7)
 });
